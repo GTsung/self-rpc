6bafd90ff31eda5385ebed5fb1dab18931d8833d
GTsung <xc_goo@163.com>
2022-01-16 00:39:27 +0800

    serviceUtil

---
+++ rpc-common/src/main/java/com/gt/rpc/util/ServiceUtil.java
@@ -0,0 +1,22 @@
+package com.gt.rpc.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author GTsung
+ * @date 2022/1/16
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ServiceUtil {
+
+    public static final String SERVICE_CONCAT_TOKEN = "#";
+
+    public static String makeServiceKey(String interfaceName, String version) {
+        String serviceKey = interfaceName;
+        if (version != null && version.trim().length() > 0) {
+            serviceKey += SERVICE_CONCAT_TOKEN.concat(version);
+        }
+        return serviceKey;
+    }
+}
