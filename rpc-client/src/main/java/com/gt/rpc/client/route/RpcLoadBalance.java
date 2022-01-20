6f4403773a13f351d91d80ab0e429682db9e6d84
GTsung <xc_goo@163.com>
2022-01-20 19:05:10 +0800

    輪詢

---
+++ rpc-client/src/main/java/com/gt/rpc/client/route/RpcLoadBalance.java
@@ -0,0 +1,46 @@
+package com.gt.rpc.client.route;
+
+import com.gt.rpc.client.handler.RpcClientHandler;
+import com.gt.rpc.protocol.RpcProtocol;
+import com.gt.rpc.util.ServiceUtil;
+import org.apache.commons.collections4.map.HashedMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author GTsung
+ * @date 2022/1/20
+ */
+public abstract class RpcLoadBalance {
+
+    protected Map<String, List<RpcProtocol>> getServiceMap(Map<RpcProtocol, RpcClientHandler> connectedServerNodes) {
+        Map<String, List<RpcProtocol>> serviceMap = new HashedMap<>();
+        if (connectedServerNodes != null && connectedServerNodes.size() > 0) {
+            connectedServerNodes.keySet().forEach(key -> {
+                key.getServiceInfoList().forEach(rpcServiceInfo -> {
+                    String serviceKey = ServiceUtil.makeServiceKey(rpcServiceInfo.getServiceName(), rpcServiceInfo.getVersion());
+                    List<RpcProtocol> rpcProtocols = serviceMap.get(serviceKey);
+                    if (rpcProtocols == null) {
+                        rpcProtocols = new ArrayList<>();
+                    }
+                    rpcProtocols.add(key);
+                    serviceMap.putIfAbsent(serviceKey, rpcProtocols);
+                });
+            });
+        }
+        return serviceMap;
+    }
+
+    /**
+     * 負載
+     * @param serviceKey
+     * @param connectedServerNodes
+     * @return
+     * @throws Exception
+     */
+    public abstract RpcProtocol route(String serviceKey, Map<RpcProtocol, RpcClientHandler> connectedServerNodes)
+            throws Exception;
+
+}
